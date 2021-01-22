61670e3dc17e85917db9fac08686b18ea5519108
nofarb <nofarb@gmail.com>
2021-01-22 08:18:45 -0500

    Update hello_server_test.go

--- hello_server_test.go
+++ hello_server_test.go
@@ -16,6 +16,13 @@ func TestGreetingSpecificDemo(t *testing.T) {
 	}
 }
 
+func TestThatShoudFail(t *testing.T) {
+	greeting := CreateGreeting("Demo1")
+	if greeting != "Hello, Demo\n" {
+		t.Errorf("Intentional Failure. Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Demo\n")
+	}
+}
+
 func TestGreetingDefault(t *testing.T) {
 	greeting := CreateGreeting("")
 	if greeting != "Hello, Guest\n" {
